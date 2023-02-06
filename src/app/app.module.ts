63087846737b8e50f5939dcc80addb6d49a078f1
sheetus <47025393+sheetus@users.noreply.github.com>
2023-02-06 20:36:14 +0200

    this portfilio is built by angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PortofiloComponent } from './portofilo/portofilo.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutmeComponent,
+    SkillsComponent,
+    PortofiloComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    NgbNavModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/portofilo/portofilo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portofilo',
+  templateUrl: './portofilo.component.html',
+  styleUrls: ['./portofilo.component.css']
+})
+export class PortofiloComponent {
+
+}
